48d7a8524ee9b0e9ed106ba89af2723190fc7460
zhengwei <zhenghaku@gmail.com>
2023-08-02 09:06:24 +0800

    add comments to to notebooks

--- README.md
+++ README.md
@@ -1,47 +1,61 @@
 # ITU JTEXT
+
 ## Description
-This project contains two main files: a freq_main_processing.py file and 
+
+This project contains two main files: a freq_main_processing.py file and
 a model_traning_eval.py file. The processing file is used to preprocess raw data
 for model training, while the model training and evaluation file converts the
-preprocessed data into a matrix format suitable for training a LightGBM model and 
+preprocessed data into a matrix format suitable for training a LightGBM model and
 evaluates the performance of the trained model.
 
 ## Files
-1. `freq_main_processing.py`: This file contains the code for processing the raw 
-data. It performs tasks such as data cleaning, feature engineering, and data 
-transformation to prepare the data for model training.
+
+1. `freq_main_processing.py`: This file contains the code for processing the raw
+   data. It performs tasks such as data cleaning, feature engineering, and data
+   transformation to prepare the data for model training.
 
 2. `model_traning_eval.py`: This file is responsible for training a LightGBM model
-using the preprocessed data. It converts the data into a matrix format that can be
-consumed by the LightGBM library. Additionally, it evaluates the performance of 
-the trained model using suitable metrics.
+   using the preprocessed data. It converts the data into a matrix format that can be
+   consumed by the LightGBM library. Additionally, it evaluates the performance of
+   the trained model using suitable metrics.
 
-## Folder 
+## Folder
+
 1. `util`: this folder contains the processors which are used in
-'freq_main_processing.py'
+   'freq_main_processing.py'
 
 ## Usage
-1. Run the `freq_main_processing.py` script to preprocess the raw data. Make sure
-to provide the necessary input data files or modify the code to fetch the data 
-from the appropriate source. The processed data will be stored in a format 
-suitable for the model training and evaluation step.
 
-2. Once the data has been processed, run the `model_traning_eval.py` script. 
-Ensure that the processed data files are accessible by the script. The script 
-will train a LightGBM model using the processed data and output the evaluation 
-results, such as accuracy, precision, recall, or any other relevant metrics.
+**NOTE:The 2 ipython not books are more readable.**
+
+1. Run the `freq_main_processing.py` script to preprocess the raw data. Make sure
+   to provide the necessary input data files or modify the code to fetch the data
+   from the appropriate source. The processed data will be stored in a format
+   suitable for the model training and evaluation step.
+
+2. Once the data has been processed, run the `model_traning_eval.py` script.
+   Ensure that the processed data files are accessible by the script. The script
+   will train a LightGBM model using the processed data and output the evaluation
+   results, such as accuracy, precision, recall, or any other relevant metrics.
+
 ## Evaluation
+
 ### Confusion Matrix
+
 ![Confusion Matrix](eval_fig/confusion_matrix.png)
 
 ### ROC Curve
+
 ![ROC Curve](eval_fig/roc.png)
 
 ### Histogram Warning Time
+
 ![Histogram Warning Time](eval_fig/histogram_warning_time.png)
 
 ### Accumulate Warning Time
+
 ![Accumulate Warning Time](eval_fig/accumulate_warning_time.png)
 
 ## Requirements
+
 - JDDB
--- freq_main_processing.py
+++ freq_main_processing.py
@@ -1,15 +1,17 @@
-# @Time : 2023/5/11 22:29 
-# @Author : zhongyu 
+# @Time : 2023/5/11 22:29
+# @Author : zhongyu
 # @File : freq_main_processing.py
+# %%
 import numpy as np
 from jddb.file_repo import FileRepo
 from jddb.processor import ShotSet
 from util.basic_processor import SliceProcessor, FFTProcessor, find_tags, AlarmTag, get_machine_tags
-from jddb.processor.basic_processors import ResamplingProcessor,TrimProcessor
+from jddb.processor.basic_processors import ResamplingProcessor, TrimProcessor
 import pandas as pd
 
+# %%
 if __name__ == '__main__':
-    # %%
+
     # choose common signal
     df_signal = pd.read_csv('ITU data - signal.csv')  # read signal csv file
     common_signal = []
@@ -20,17 +22,20 @@ if __name__ == '__main__':
                 ~target_row['C-Mod MDSplus Tag'].isna().values[0]:
             common_signal.append(signal_name)
     # change common_signal to different sub list according to whether it belongs to array mirnove, sxr, axuv.
-    mir_name_list = find_tags('poloidal', common_signal) + find_tags('toroidal Mir', common_signal)
+    mir_name_list = find_tags('poloidal', common_signal) + \
+        find_tags('toroidal Mir', common_signal)
     sxr_name_list = find_tags('soft', common_signal)
     axuv_name_list = find_tags('AXUV', common_signal)
-    basic_name_list = list(set(common_signal) - set(mir_name_list + sxr_name_list + axuv_name_list))
+    basic_name_list = list(set(common_signal) -
+                           set(mir_name_list + sxr_name_list + axuv_name_list))
     # choose machine for processing and get machine tags by common name list
     machine_name = 'J-TEXT'
-    basic_machine_tags = get_machine_tags(machine_name, basic_name_list, df_signal)
+    basic_machine_tags = get_machine_tags(
+        machine_name, basic_name_list, df_signal)
     mir_machine_tags = get_machine_tags(machine_name, mir_name_list, df_signal)
     # %%
-    source_file_repo = FileRepo('..//FileRepo//processed_zy//$shot_2$00//')
-    train_file_repo = FileRepo('..//FileRepo//train_file//$shot_2$00//')
+    source_file_repo = FileRepo('.//data//jtext_data//$shot_2$00//')
+    train_file_repo = FileRepo('.//data//jtext_data_train//$shot_2$00//')
     # create a valid shot set with a file, the valid shots should contain target tags and enough flattop time.
     source_shotset = ShotSet(source_file_repo)
     shot_list = source_shotset.shot_list
@@ -39,11 +44,13 @@ if __name__ == '__main__':
     valid_shots = []  # Initialize an empty list to store valid shots
     for shot in shot_list:
         all_tags = list(source_shotset.get_shot(shot).tags)
-        last_time = list(source_file_repo.read_labels(shot, ['DownTime']).values())
+        last_time = list(source_file_repo.read_labels(
+            shot, ['DownTime']).values())
         # Check if all target tags are present in the shot's tags and last_time of shot is greater than 0.2s
         if all(tag in all_tags for tag in targ_tags) & (last_time[0] > 0.2):
             valid_shots.append(shot)
-    valid_shotset = ShotSet(source_file_repo, valid_shots)  # Create a new ShotSet object using the valid shots
+    # Create a new ShotSet object using the valid shots
+    valid_shotset = ShotSet(source_file_repo, valid_shots)
 
     # 1. FFT processing for max frequency and amplitude of mirnov signals
     # %%
@@ -61,15 +68,18 @@ if __name__ == '__main__':
         processed_shotset = processed_shotset.process(
             processor=FFTProcessor(),
             input_tags=["sliced_MA_{}".format(mir_tag)],
-            output_tags=[["fft_amp_{}".format(mir_tag), "fft_fre_{}".format(mir_tag)]],
+            output_tags=[["fft_amp_{}".format(
+                mir_tag), "fft_fre_{}".format(mir_tag)]],
             save_repo=train_file_repo)
 
     # %%
     # 2. remove redundant tags and keep tags for model training
     shot_list = processed_shotset.shot_list
     all_tags = list(processed_shotset.get_shot(shot_list[0]).tags)
-    fft_tag = find_tags('fft_', all_tags)  # tags coming from process should be included like fft and so on.
-    keep_tags = basic_machine_tags + fft_tag  # mir signals have been processed, they shouldn't be kept.
+    # tags coming from process should be included like fft and so on.
+    fft_tag = find_tags('fft_', all_tags)
+    # mir signals have been processed, they shouldn't be kept.
+    keep_tags = basic_machine_tags + fft_tag
     processed_shotset = processed_shotset.remove_signal(tags=keep_tags, keep=True,
                                                         save_repo=train_file_repo)
 
@@ -84,7 +94,8 @@ if __name__ == '__main__':
 
     # %%
     # 4. trim  signal
-    common_tags = basic_name_list + fft_tag  # change the machine tags to common names
+    # change the machine tags to common names
+    common_tags = basic_name_list + fft_tag
     processed_shotset = processed_shotset.process(
         TrimProcessor(),
         input_tags=[keep_tags],
@@ -97,7 +108,8 @@ if __name__ == '__main__':
     # %%
     # 5. add disruption labels for each time point as a signal called alarm_tag
     processed_shotset = processed_shotset.process(
-        processor=AlarmTag(lead_time=0.1, disruption_label="IsDisrupt", downtime_label="DownTime"),
+        processor=AlarmTag(
+            lead_time=0.1, disruption_label="IsDisrupt", downtime_label="DownTime"),
         input_tags=["plasma current"],
         output_tags=["alarm_tag"],
         save_repo=train_file_repo)
